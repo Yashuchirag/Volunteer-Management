7d9c8bb1b90c312fc1061b9859861446950025eb
Yashuchirag <chiragchandrashekar@gmail.com>
2024-04-18 14:18:28 -0600

    Updated App.js

--- Web_app/src/main/App.js
+++ Web_app/src/main/App.js
@@ -11,39 +11,45 @@ const App = () => {
         {
             name: 'Pilot Event',
             description: 'Getting the event started for Volunteers Management platform',
-            date: '04/10/2024',
-            time: '9:00 pm',
+            date: '04/18/2024',
+            time: '6:00 pm',
             volunteersNeeded: 4
         },
         {
-            name: 'Pilot Event',
-            description: 'Getting the event started for Volunteers Management platform',
-            date: '04/10/2024',
-            time: '9:00 pm',
-            volunteersNeeded: 4
+            name: 'Community Cleanup Day',
+            description: 'Join us for a community cleanup event to keep our neighborhood clean and beautiful.',
+            date: '04/19/2024',
+            time: '10:00 am',
+            volunteersNeeded: 10
         },
         {
-            name: 'Pilot Event',
-            description: 'Getting the event started for Volunteers Management platform',
-            date: '04/10/2024',
-            time: '9:00 pm',
-            volunteersNeeded: 4
+            name: 'Children\'s Charity Fun Fair',
+            description: 'Help make a difference in the lives of children by volunteering at our charity fun fair. Fun games, food, and entertainment for a good cause!',
+            date: '04/20/2024',
+            time: '11:00 am',
+            volunteersNeeded: 20
+        },    
+        {
+            name: 'Environmental Awareness Workshop',
+            description: 'Join us for an educational workshop on environmental conservation and sustainability. Learn how you can contribute to protecting our planet.',
+            date: '04/25/2024',
+            time: '2:00 pm',
+            volunteersNeeded: 15
         },
         {
-            name: 'Pilot Event',
-            description: 'Getting the event started for Volunteers Management platform',
-            date: '04/10/2024',
-            time: '9:00 pm',
-            volunteersNeeded: 4
-        },
+            name: 'Animal Shelter Adoption Event',
+            description: 'Help find loving homes for shelter animals by volunteering at our adoption event. Spend time with adorable pets and assist potential adopters.',
+            date: '04/30/2024',
+            time: '1:00 pm',
+            volunteersNeeded: 8
+        },   
         {
-            name: 'Pilot Event',
-            description: 'Getting the event started for Volunteers Management platform',
-            date: '04/10/2024',
-            time: '9:00 pm',
-            volunteersNeeded: 4
+            name: 'Senior Citizen Social Hour',
+            description: 'Brighten the day of our senior citizens by volunteering at our social hour event. Engage in conversation, play games, and spread joy!',
+            date: '05/05/2024',
+            time: '3:00 pm',
+            volunteersNeeded: 12
         },
-        // Add more event objects as needed
     ];
 
     const handleLogin = (email) => {
