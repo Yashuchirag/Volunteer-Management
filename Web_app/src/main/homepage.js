baa1a30cc87625477f7c844867f2ba873481c941
Yashuchirag <chiragchandrashekar@gmail.com>
2024-04-18 14:25:05 -0600

    Updated Homepage.js

--- Web_app/src/main/homepage.js
+++ Web_app/src/main/homepage.js
@@ -5,15 +5,15 @@ const Homepage = ({ user, events }) => {
     return (
         <div className="homepage-container">
             <div className="heading">Welcome, {user}! </div>
-            <div className="subtitle">List of Events:</div>
+            <div className="subtitle"><b>Upcoming Events:</b></div>
             <table className="event-table">
                 <thead>
                     <tr>
-                        <th>Event Name</th>
-                        <th>Event Description</th>
-                        <th>Event Date</th>
-                        <th>Event Time</th>
-                        <th>Volunteers needed</th>
+                        <th style={{ width: '5cm' }}>Event Name</th>
+                        <th style={{ width: '10cm' }}>Event Description</th>
+                        <th style={{ width: '2.5cm' }}>Event Date</th>
+                        <th style={{ width: '2.5cm' }}>Event Time</th>
+                        <th style={{ width: '3.5cm' }}>Volunteers needed</th>
                     </tr>
                 </thead>
                 <tbody>
